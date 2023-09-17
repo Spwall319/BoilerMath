752df3f5338f13b136cbbb150d96bf6f8a82e56f
Spwall319 <143623456+Spwall319@users.noreply.github.com>
2023-09-17 09:55:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from PIL import Image
 #digit.create_model()
 #symbol.create_model_symbol()
 
-img = cv2.imread('C:\\Users\\spwal\\Downloads\\equ2.png')
+img = cv2.imread('INSERT FILE PATH TO IMAGE OF EQUATION')
 
 s1 = str(digit.predictDigit(img[:,0:img.shape[1]//3]))
 s1 += symbol.predictSymbol(img[:,img.shape[1]//3:2*img.shape[1]//3])
 s1 += str(digit.predictDigit(img[:,2*img.shape[1]//3:img.shape[1]]))
 
 print(s1)
-#print(openAI.entry(s1))
+print(openAI.entry(s1))
